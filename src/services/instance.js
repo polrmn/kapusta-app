23940881ae3ce2c051ac3c6e91aecd4733f67cbb
polrmn <polrmn@gmail.com>
2023-02-27 16:00:21 +0200

    create instance

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-export const instanse = axios.create({
+export const instance = axios.create({
   baseURL: 'https://kapusta-backend.goit.global/',
 });
 
 // Utility to add JWT
 export const setAuthHeader = token => {
-  apiServices.defaults.headers.common.Authorization = `Bearer ${token}`;
+  instance.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
 // Utility to remove JWT
 export const clearAuthHeader = () => {
-  apiServices.defaults.headers.common.Authorization = '';
+  instance.defaults.headers.common.Authorization = '';
 };
