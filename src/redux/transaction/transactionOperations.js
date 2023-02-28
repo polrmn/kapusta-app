7b7f9ecf97e1381415d3814137e1a3a029eaa6f1
polrmn <polrmn@gmail.com>
2023-02-28 11:41:10 +0200

    merge services

--- src/redux/transaction/transactionOperations.js
+++ src/redux/transaction/transactionOperations.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { instance } from 'services/instance';
+import { getPeriodDataAPI } from 'services/transactionService';
 
 export const thunkName = createAsyncThunk(
   'signature/thunkName',
@@ -15,7 +15,7 @@ export const getTransactionsThunk = createAsyncThunk(
   'transaction/byDate',
   async (date, { rejectWithValue }) => {
     try {
-      const { data } = await instance.get(`transaction/${date}`);
+      const data = await getPeriodDataAPI(date);
       return data;
     } catch (error) {
       return rejectWithValue(error.message);
