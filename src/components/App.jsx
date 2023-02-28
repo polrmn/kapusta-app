fb2214f51e6ce3a0acb7f8e0b5c1014672f0a147
Lika <14dubinenkoanzhelika@gmail.com>
2023-02-28 19:59:32 +0200

    add modal and header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,35 @@
+import { ModalContextProvider } from 'context/ ModalContext';
 import { LoginPage } from 'pages/LoginPage/LoginPage';
 import { RegisterPage } from 'pages/RegisterPage/RegisterPage';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { ModalHeader } from './ModalHeader/ModalHeader';
 // import { PublicRoute } from './PublicRoute/PublicRoute';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        {/* <Route path="/" element={<PublicRoute />}> */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        {/* </Route> */}
-        {/* <Route path="/" element={<PrivateRoute />}> */}
-        <Route path="/home">
+    <ModalContextProvider>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          {/* <Route path="/" element={<PublicRoute />}> */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          {/* </Route> */}
+          {/* <Route path="/" element={<PrivateRoute />}> */}
+          {/* <Route path="/home">
           <Route path="expenses" element={<ExpensesHome />} />
           <Route path="income" element={<IncomeHome />} />
         </Route>
         <Route path="/reports">
           <Route path="expenses" element={<ExpensesReports />} />
           <Route path="income" element={<IncomeReports />} />
+        </Route> */}
+          {/* </Route> */}
         </Route>
-        {/* </Route> */}
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <ModalHeader />
+    </ModalContextProvider>
   );
 };
 
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, Outlet } from 'react-router-dom';
 import css from './sharedLayout.module.scss';
 import { ReactComponent as ReactLogo } from '../../images/logo-header.svg';
+import { ModalContext } from 'context/ ModalContext';
 
 export const SharedLayout = () => {
+  const [, setOpen] = useContext(ModalContext);
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
   return (
     <>
       <header className={css.header}>
         <div className={css.container}>
-          <div className={css.logo}>
-            <a href="#" className={css.headerLogo}>
-              <ReactLogo />
-            </a>
+          <div className={css.containerHeader}>
+            <div className={css.logo}>
+              <Link to="/home" className={css.headerLogo}>
+                <ReactLogo />
+              </Link>
+            </div>
+            <div className={css.headerExit}>
+              <span className={css.headerUserLetter}>L</span>
+              <p className={css.headerUser}>Lika</p>
+              <button
+                className={css.headerButtonLogout}
+                onClick={handleOpenModal}
+              >
+                Exit
+              </button>
+            </div>
           </div>
         </div>
       </header>
