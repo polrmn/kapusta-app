57aa0d6bc9396d03ade7fad059c3682af8fa3b0d
polrmn <polrmn@gmail.com>
2023-02-28 15:36:40 +0200

    add commonButton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,9 @@
-import Button from 'commons/Button';
 import Authorization from './auth/Authorization';
 
 const App = () => {
   return (
     <>
       <Authorization />
-      <Button active={true} content={'log in'} />
-      <Button active={false} content={'registration'} />
     </>
   );
 };
