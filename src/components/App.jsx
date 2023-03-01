3a7e03cba49acc164b9e5725654062d85e0ce3c1
KolyaSeneta <114915582+KolyaSeneta@users.noreply.github.com>
2023-03-01 13:28:32 +0200

    mergen dev

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,16 @@ import { SharedLayout } from './SharedLayout/SharedLayout';
 import Authorization from './auth/Authorization';
 
 const App = () => {
-<<<<<<< HEAD
   return <>test</>;
-=======
   return (
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          {/* <Route path="/" element={<PublicRoute />}> */}
-          <Route path="/login" element={<Authorization type='login'/>} />
-          <Route path="/register" element={<Authorization type='signup' />} />
-          {/* </Route> */}
-          {/* <Route path="/" element={<PrivateRoute />}> */}
-          {/* <Route path="/home">
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        {/* <Route path="/" element={<PublicRoute />}> */}
+        <Route path="/login" element={<Authorization type="login" />} />
+        <Route path="/register" element={<Authorization type="signup" />} />
+        {/* </Route> */}
+        {/* <Route path="/" element={<PrivateRoute />}> */}
+        {/* <Route path="/home">
           <Route path="expenses" element={<ExpensesHome />} />
           <Route path="income" element={<IncomeHome />} />
         </Route>
@@ -23,12 +21,11 @@ const App = () => {
           <Route path="expenses" element={<ExpensesReports />} />
           <Route path="income" element={<IncomeReports />} /> 
         </Route> */}
-          {/* </Route> */}
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+        {/* </Route> */}
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
->>>>>>> dev
 };
 
 export default App;
