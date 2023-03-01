0e6af7aa2a6d99b28348b5dd0152d7a070c206f2
polrmn <polrmn@gmail.com>
2023-03-01 11:29:39 +0200

    merge login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,10 @@
-import { ModalContextProvider } from 'context/ ModalContext';
-import { LoginPage } from 'pages/LoginPage/LoginPage';
-import { RegisterPage } from 'pages/RegisterPage/RegisterPage';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { ModalHeader } from './ModalHeader/ModalHeader';
 // import { PublicRoute } from './PublicRoute/PublicRoute';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import Authorization from './auth/Authorization';
 
 const App = () => {
   return (
-    <ModalContextProvider>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           {/* <Route path="/" element={<PublicRoute />}> */}
@@ -29,8 +24,6 @@ const App = () => {
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-      {/*<ModalHeader />*/}
-    </ModalContextProvider>
   );
 };
 
--- src/components/auth/AuthorizationForm.jsx
+++ src/components/auth/AuthorizationForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 import { useFormik } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
