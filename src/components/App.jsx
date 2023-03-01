321bca076ecc7f3668660118d7f048d030d7e3e9
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2023-03-01 14:31:59 +0200

    merge i

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,29 +9,25 @@ import { Expenses } from './Expenses/Expenses';
 
 const App = () => {
   return (
-    // <ModalContextProvider>
-    //   <Routes>
-    //     <Route path="/" element={<SharedLayout />}>
-    //       {/* <Route path="/" element={<PublicRoute />}> */}
-    //       <Route path="/login" element={<LoginPage />} />
-    //       <Route path="/register" element={<RegisterPage />} />
-    //       {/* </Route> */}
-    //       {/* <Route path="/" element={<PrivateRoute />}> */}
-    //       {/* <Route path="/home">
-    //       <Route path="expenses" element={<ExpensesHome />} />
-    //       <Route path="income" element={<IncomeHome />} />
-    //     </Route>
-    //     <Route path="/reports">
-    //       <Route path="expenses" element={<ExpensesReports />} />
-    //       <Route path="income" element={<IncomeReports />} />
-    //     </Route> */}
-    //       {/* </Route> */}
-    //     </Route>
-    //     <Route path="*" element={<Navigate to="/" />} />
-    //   </Routes>
-    //   <ModalHeader />
-    // </ModalContextProvider>
-    <Expenses />
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        {/* <Route path="/" element={<PublicRoute />}> */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        {/* </Route> */}
+        {/* <Route path="/" element={<PrivateRoute />}> */}
+        {/* <Route path="/home"> */}
+        {/* <Route path="expenses" element={<Expenses />} /> */}
+        {/* <Route path="income" element={<IncomeHome />} /> */}
+        {/* </Route> */}
+        {/* <Route path="/reports">
+          <Route path="expenses" element={<ExpensesReports />} />
+          <Route path="income" element={<IncomeReports />} />
+        </Route> */}
+        {/* </Route> */}
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 };
 
