698bc6b56754fd464faae37370bf9eaa4c699f2a
KolyaSeneta <114915582+KolyaSeneta@users.noreply.github.com>
2023-03-01 13:21:39 +0200

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,5 @@
-import { ChartExpenses } from './Chart/ChartExpenses/ChartExpenses';
-import { ChartIncome } from './Chart/ChartIncome/ChartIncome';
-import { ChartExpensesMobile } from './Chart/ChartExpensesMobile/ChartExpensesMobile';
-import { ChartIncomeMobile } from './Chart/ChartIncomeMobile/ChartIncomeMobile';
 const App = () => {
-  return (
-    <>
-      test
-      <ChartExpenses />
-      <ChartIncome />
-      <ChartExpensesMobile />
-      <ChartIncomeMobile />
-    </>
-  );
+  return <>test</>;
 };
 
 export default App;
