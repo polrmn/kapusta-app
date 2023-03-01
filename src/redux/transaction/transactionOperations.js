81dffe0e94f4cf413893c2793f4ca0272b571bc8
ROMAN POLISHCHUK <114829451+polrmn@users.noreply.github.com>
2023-03-01 18:38:25 +0200

    Update transactionOperations.js

--- src/redux/transaction/transactionOperations.js
+++ src/redux/transaction/transactionOperations.js
@@ -4,18 +4,18 @@ import { getExpenseAPI, getIncomeAPI } from 'services/transactionService';
 
 export const thunkName = createAsyncThunk(
   'signature/thunkName',
-  async (data, { rejectWithValue }) => {
-    try {
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
+@@ -11,47 +12,38 @@ export const thunkName = createAsyncThunk(
   }
 );
 
+export const getTransactionsThunk = createAsyncThunk(
+  'transaction/byDate',
+  async (date, { rejectWithValue }) => {
 export const getIncome = createAsyncThunk(
   'transition/getIncome',
   async (_, { rejectWithValue }) => {
     try {
+      const data = await getPeriodDataAPI(date);
       const data = await getIncomeAPI();
       return data;
     } catch (error) {
@@ -24,10 +24,17 @@ export const getIncome = createAsyncThunk(
   }
 );
 
+export const addBalanceThunk = createAsyncThunk(
+  'user/balance',
+  async (balance, thunkApi) => {
 export const getExpense = createAsyncThunk(
   'transition/getExpense',
   async (_, { rejectWithValue }) => {
     try {
+      // const newBalance = await addBalanceRequest(balance);
+      // return newBalance;
+    } catch (e) {
+      return thunkApi.rejectWithValue(e);
       const data = await getExpenseAPI();
       return data;
     } catch (error) {
@@ -47,3 +54,24 @@ export const getTransactionsThunk = createAsyncThunk(
     }
   }
 );
+// export const addBalance = createAsyncThunk(
+//   'user/balance',
+//   async (balance, thunkApi) => {
+//     try {
+//       const userBalance = await addBalanceRequest(balance);
+//       userBalance?.token && localStorage.setItem('token', userBalance.token);
+//       return userBalance;
+//     } catch (e) {
+//       return thunkApi.rejectWithValue(e.message);
+//     }
+//   }
+// );
+// export const addBalane = createAsyncThunk("user/balance", async (balanceData, thunkAPI) => {
+//     try {
+//         const { data } = await axios.patch("/balance", { ...balanceData });
+//         return data;
+//     } catch (e) {
+//         return thunkAPI.rejectWithValue(e.message);
+//     }
+// }
+// );
