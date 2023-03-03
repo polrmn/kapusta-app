bf1eb5b96d8b6487f8a20f32531e5ffa1ca124b4
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2023-03-03 17:27:14 +0200

    balance styles

--- src/components/BalancePanel/BalancePanel.jsx
+++ src/components/BalancePanel/BalancePanel.jsx
@@ -138,66 +138,6 @@ function BalancePanel() {
           </Modal>
         </section>
       )}
-      {/* <section className={style.balance}>
-        <div className={style.balanceBack}>
-          <button type="button" className={style.balanceBtnBack}>
-            <Link className={style.balanceLinkBack} to="/">to transaction</Link>
-          </button>
-        </div>
-        <div className={style.balanceMain}>
-          <button type="button" className={style.balanceBtnMain}>
-            <Link className={style.balanceLinkMain} to="/">Main page</Link>
-          </button>
-        </div>
-        <div className={style.balanceReports}>
-          <button type="button" className={style.balanceBtnReport}>
-            <Link className={style.balanceLinkReport} to="/reports">
-              Reports
-            </Link>
-          </button>
-        </div>
-        <div className={style.balanceCal}>
-          <p className={style.balanceCalPara}>Current period:</p>
-          <div className={style.balanceCalInner}>
-            <button type='button' className={style.balanceCalDec}></button>
-            <p className={style.balanceCalProp}>November 2019</p>
-            <button type='button' className={style.balanceCalInc}></button>
-          </div>
-        </div>
-        <div className={style.balanceInnerBlock}>
-          <p className={style.balancePara}>Balance:</p>
-          <div className={style.balanceAdd}>
-            <p className={style.balanceProp}>{balAnce} coins</p>
-            <button
-              onClick={() => setShowModal(true)}
-              className={style.balanceBtnAdd}
-            >
-              Add balance
-            </button>
-          </div>
-          <Modal active={showModal} setActive={setShowModal}>
-            <form
-              action=""
-              onSubmit={onClickConfirm}
-              className={style.balanceForm}
-            >
-              <label>
-                <p className={style.balanceModalPara}>Balance:</p>
-                <input
-                  className={style.balanceModalInput}
-                  type="text"
-                  name="balance"
-                  value={balance}
-                  onChange={balanceChange}
-                />
-              </label>
-              <button className={style.balanceModalBtn} type="submit">
-                Confirm
-              </button>
-            </form>
-          </Modal>
-        </div>
-      </section> */}
     </>
   );
 };
